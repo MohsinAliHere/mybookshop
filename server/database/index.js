9d3de67e52269a6f990ec2d9a19fcdb465ccfc80
Mohsin Ali Khan ✪ <mohsinalihere@gmail.com>
2024-07-28 09:03:43 -0700

    Update index.js

--- server/database/index.js
+++ server/database/index.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const Connect_Db = async () => {
 	try {
 		const db = await mongoose.connect(process.env.DB_STRING);
-		console.log("Database Connecting Succesfully");
+		console.log("Database Connecting Successfully");
 	} catch (error) {
 		console.log("Database Connecting Failed", error.message);
 	}

277ef72cb88f34a33b975d5503096c62c1dc35fa
Mohsin Ali Khan ✪ <mohsinalihere@gmail.com>
2024-07-28 15:10:48 -0700

    welcome api page

--- server/index.js
+++ server/index.js
@@ -11,6 +11,36 @@ Connect_Db();
 app.use(express.json());
 
 // routes
+
+app.get("/", (req, res) => {
+	res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Book Shop</title>
+        <style>
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+          }
+          h1 {
+            font-size: 3em;
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>My Book Shop</h1>
+      </body>
+    </html>
+  `);
+});
+
 app.use(AuthRoutes);
 
 app.listen(PORT, () => console.log("Running on server " + PORT));
